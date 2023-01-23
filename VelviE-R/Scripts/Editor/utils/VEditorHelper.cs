a00961321ba01ce8202cdf33ae8083cfe2141df1
Cattywampus <64100867+breadnone@users.noreply.github.com>
2023-01-23 16:10:49 +0700

    Update VEditorHelper.cs

--- VelviE-R/Scripts/Editor/utils/VEditorHelper.cs
+++ VelviE-R/Scripts/Editor/utils/VEditorHelper.cs
@@ -6,11 +6,96 @@ using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 using UnityEditorInternal;
 using System;
+using VelvieR;
 
 namespace VIEditor
 {
     public class VEditorFunc
     {
+        public static VelvieDialogue[] EditorGetVDialogues()
+        {
+            var t = GameObject.FindGameObjectsWithTag("VelvieRoid");
+
+            if(t == null || t.Length == 0)
+                return null;
+
+            List<VelvieDialogue> dials = new List<VelvieDialogue>();
+
+            for(int i = 0; i < t.Length; i++)
+            {
+                var com = t[i].GetComponent<VelvieDialogue>();
+
+                if(com != null)
+                {
+                    dials.Add(com);
+                }
+            }
+            
+            return dials.ToArray();
+        }
+        public static VCoreUtil[] EditorGetVCoreUtils()
+        {
+            var t = GameObject.FindGameObjectsWithTag("VelvieRoid");
+
+            if(t == null || t.Length == 0)
+                return null;
+
+            List<VCoreUtil> dials = new List<VCoreUtil>();
+
+            for(int i = 0; i < t.Length; i++)
+            {
+                var com = t[i].GetComponent<VCoreUtil>();
+
+                if(com != null)
+                {
+                    dials.Add(com);
+                }
+            }
+            
+            return dials.ToArray();
+        }
+        public static VStageUtil[] EditorGetVStageUtils()
+        {
+            var t = GameObject.FindGameObjectsWithTag("VelvieRoid");
+
+            if(t == null || t.Length == 0)
+                return null;
+
+            List<VStageUtil> dials = new List<VStageUtil>();
+
+            for(int i = 0; i < t.Length; i++)
+            {
+                var com = t[i].GetComponent<VStageUtil>();
+
+                if(com != null)
+                {
+                    dials.Add(com);
+                }
+            }
+            
+            return dials.ToArray();
+        }
+        public static VMenuOption[] EditorGetVMenuUtils()
+        {
+            var t = GameObject.FindGameObjectsWithTag("VelvieRoid");
+
+            if(t == null || t.Length == 0)
+                return null;
+
+            List<VMenuOption> dials = new List<VMenuOption>();
+
+            for(int i = 0; i < t.Length; i++)
+            {
+                var com = t[i].GetComponent<VMenuOption>();
+
+                if(com != null)
+                {
+                    dials.Add(com);
+                }
+            }
+            
+            return dials.ToArray();
+        }
         public static void AssignAndAddTag(GameObject obj, string tag = "VelvieRoid")
         {
             if(obj == null)
