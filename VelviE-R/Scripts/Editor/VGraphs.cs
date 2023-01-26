71e20f0bb090a9cb3aa2251fa411ebe9015a6e81
Cattywampus <64100867+breadnone@users.noreply.github.com>
2023-01-26 21:12:17 +0700

    Update VGraphs.cs

--- VelviE-R/Scripts/Editor/VGraphs.cs
+++ VelviE-R/Scripts/Editor/VGraphs.cs
@@ -9,77 +9,9 @@ using System.Linq;
 using VelvieR;
 using UnityEditor.UIElements;
 using UnityEditor.SceneManagement;
-using System.Reflection;
-using System.Globalization;
 
 namespace VIEditor
 {
-    class SquareResizer : MouseManipulator
-    {
-        private Vector2 m_Start;
-        protected bool m_Active;
-
-        public SquareResizer()
-        {
-            activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse });
-            m_Active = false;
-        }
-
-        protected override void RegisterCallbacksOnTarget()
-        {
-            target.RegisterCallback<MouseDownEvent>(OnMouseDown);
-            target.RegisterCallback<MouseMoveEvent>(OnMouseMove);
-            target.RegisterCallback<MouseUpEvent>(OnMouseUp);
-        }
-
-        protected override void UnregisterCallbacksFromTarget()
-        {
-            target.UnregisterCallback<MouseDownEvent>(OnMouseDown);
-            target.UnregisterCallback<MouseMoveEvent>(OnMouseMove);
-            target.UnregisterCallback<MouseUpEvent>(OnMouseUp);
-        }
-
-        protected void OnMouseDown(MouseDownEvent e)
-        {
-            if (m_Active)
-            {
-                e.StopImmediatePropagation();
-                return;
-            }
-
-            if (CanStartManipulation(e))
-            {
-                m_Start = e.localMousePosition;
-
-                m_Active = true;
-                target.CaptureMouse();
-                e.StopPropagation();
-            }
-        }
-
-        protected void OnMouseMove(MouseMoveEvent e)
-        {
-            if (!m_Active || !target.HasMouseCapture())
-                return;
-
-            Vector2 diff = e.localMousePosition - m_Start;
-
-            target.parent.style.height = target.parent.layout.height + diff.x;
-            target.parent.style.width = target.parent.layout.width + diff.x;
-
-            e.StopPropagation();
-        }
-
-        protected void OnMouseUp(MouseUpEvent e)
-        {
-            if (!m_Active || !target.HasMouseCapture() || !CanStopManipulation(e))
-                return;
-
-            m_Active = false;
-            target.ReleaseMouse();
-            e.StopPropagation();
-        }
-    }
     public class VGraphs : GraphViewEditorWindow
     {
         public VViews graphView { get; set; }
