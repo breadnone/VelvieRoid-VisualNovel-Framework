35af82595a5aad783f54b65f594ffa31866f8985
Cattywampus <64100867+breadnone@users.noreply.github.com>
2023-01-27 01:09:04 +0700

    Revert "DebugMode menu"
    This reverts commit b420cafff1dabc09fc6f37d151d9ebb27f65570c.

--- VelviE-R/Scripts/Editor/utils/VToolbars.cs
+++ VelviE-R/Scripts/Editor/utils/VToolbars.cs
@@ -520,46 +520,9 @@ namespace VIEditor
                 };
             }
 
-            bwBtn.text = "BackgroundScheduler";
+            bwBtn.text = "Background Scheduler";
             bwBtn.style.backgroundColor = Color.grey;
             toolbarThree.Add(bwBtn);
-
-            var dropDmode = new DropdownField();
-            dropDmode.choices = new List<string>{"NormalMode", "DebugMode"};
-
-            if(!EditorPrefs.HasKey("v-Debug-v-Mode"))
-            {
-                EditorPrefs.SetBool("v-Debug-v-Mode", false);
-            }
-
-            bool tmpbool = EditorPrefs.GetBool("v-Debug-v-Mode");
-
-            if(tmpbool)
-            {
-                dropDmode.value = "DebugMode";
-            }
-            else
-            {
-                dropDmode.value = "NormalMode";
-            }
-
-            if(!PortsUtils.PlayMode)
-            {
-                dropDmode.RegisterValueChangedCallback(x =>
-                {
-                    if(x.newValue == "DebugMode")
-                    {
-                        EditorPrefs.SetBool("v-Debug-v-Mode", true);
-                        PortsUtils.VGraph.DrawDebugWindow(true);
-                    }
-                    else
-                    {
-                        EditorPrefs.SetBool("v-Debug-v-Mode", false);
-                        PortsUtils.VGraph.DrawDebugWindow(false);
-                    }
-                });
-            }
-            toolbarThree.Add(dropDmode);
 
             Toolbars.Add(toolbarThree);
 
